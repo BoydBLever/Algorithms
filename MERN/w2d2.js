34a2d6dfd8bff738dc4413cab2021052e5609d61
Boyd Lever <boydlever@stanfordhacker.com>
2023-01-17 09:20:30 -0800

    Uncommented

--- MERN/w2d2.js
+++ MERN/w2d2.js
@@ -27,8 +27,4 @@ const intersect = (arrLeft, arrRight) => {
     return outArr;
 }
 
-// console.log(intersect([2,4,7,9,10],[2,3,5,7,9,10]));
-// console.log(intersect([1,1,4,5,8],[1,1,1,5,6,8]));
-// console.log(intersect([1,3,5,7,9],[2,4,6,8,10]));
-
 console.log(intersect([-9, 1, 3, 4, 4, 4, 4, 4, 4, 5, 7], [-5, 0, 1, 1, 1, 1, 1, 2, 4, 4, 6, 7]));
