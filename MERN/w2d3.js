7be437de669cbbf13153ec95db1a212489d6a002
Boyd Lever <boydlever@stanfordhacker.com>
2023-01-18 08:57:39 -0800

    No change

--- MERN/w2d3.js
+++ MERN/w2d3.js
@@ -1,21 +1,24 @@
-function Union(arr1, arr2, m, n) {
+function returnSortedWithoutIntersection(arr1, arr2, m, n) {
+    
     let i = 0, j = 0;
     while (i < m && j < n) {
         if (arr1[i] < arr2[j])
-            console.log(arr1[i++] + " ");
+        console.log(arr1[i++] + " ");
         else if (arr2[j] < arr1[i])
-            console.log(arr2[j++] + " ");
+        console.log(arr2[j++] + " ");
         else {
             console.log(arr2[j++] + " ");
             i++;
         }
     }
+    
     /* Print remaining elements of
     the larger array */
+    
     while (i < m)
-        console.log(arr1[i++] + " ");
+    console.log(arr1[i++] + " ");
     while (j < n)
-        console.log(arr2[j++] + " ");
+    console.log(arr2[j++] + " ");
     return 0;
 }
 
@@ -23,4 +26,4 @@ let arr1 = [1, 2, 4, 5, 6];
 let arr2 = [2, 3, 5, 7];
 let m = arr1.length;
 let n = arr2.length;
-Union(arr1, arr2, m, n);
+returnSortedWithoutIntersection(arr1, arr2, m, n);
