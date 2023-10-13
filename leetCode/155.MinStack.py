3de23ffacdf986e665768a1d07b5bb8b2f469adf
Boyd Lever <boydlever@stanfordhacker.com>
2023-10-12 18:44:44 -0700

    155. Min Stack Solution in Python

---
+++ leetCode/155.MinStack.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/min-stack/
+class MinStack:
+
+    # 1. Initialize two stacks: one for values and one for minimums.
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    # 1. Push the value onto the main stack.
+    # 2. If the min_stack is empty or the current value is less than or equal to the top of min_stack, push it onto min_stack.
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    # 1. Pop the value from the main stack.
+    # 2. If the popped value is equal to the top of min_stack, pop it from min_stack as well.
+    def pop(self) -> None:
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+        self.stack.pop()
+
+    # 1. Return the top of the main stack.
+    def top(self) -> int:
+        return self.stack[-1]
+
+    # 1. Return the top of the min_stack.
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
