8a5f88381a8079dffbec6f283eb40f23a2e94b92
Boyd Lever <boydlever@stanfordhacker.com>
2023-10-12 19:03:15 -0700

    Add Neetcode solution

--- leetCode/155.MinStack.py
+++ leetCode/155.MinStack.py
@@ -35,3 +35,25 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+#Neetcode solution
+# class MinStack:
+
+#     def __init__(self):
+#         self.stack = []
+#         self.minStack = []
+
+#     def push(self, val:int) -> None:
+#         self.stack.append(val)
+#         val = min(val, self.minStack[-1] if self.minStack val)
+#         self.minStack.append(val)
+
+#     def pop(self) -> None:
+#         self.stack.pop()
+#         self.minStack.pop()
+
+#     def top(self) -> int:
+#         return self.stack[-1]
+    
+#     def getMin(self) -> int:
+#         return self.minStack[-1]
