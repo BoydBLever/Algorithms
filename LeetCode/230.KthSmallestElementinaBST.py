41b3e7724d71b193fe23b6f7c10693526609f261
Boyd Lever <boydlever@stanfordhacker.com>
2023-10-15 15:59:15 -0700

    230. Kth Smallest Element in a BST

---
+++ LeetCode/230.KthSmallestElementinaBST.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+# Neetcode solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        # use a stack iteratively
+        n = 0
+        stack = []
+        cur = root  # pointer
+
+        while cur or stack: #Neetcode uses "and"
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+
+            cur = stack.pop()
+            n += 1
+            if n == k:
+                return cur.val
+            cur = cur.right
