b0f4e222baf1f798b393c231e55702e43628ee98
Boyd Lever <boydlever@stanfordhacker.com>
2023-10-16 14:46:08 -0700

    295. Find Median from Data Stream

---
+++ LeetCode/295.FindMedianfromDataStream.py
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/find-median-from-data-stream/
+# Neetcode solution
+
+import heapq
+class MedianFinder:
+
+    def __init__(self):
+        # two heaps, Large, small, minheap, maxheap
+        # heaps should be equal size
+        self.small, self.large = [], []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small, -1 * num)
+
+        # make sure every num small is <= every num in large
+        if (self.small and self.large and
+         (-1 * self.small[0]) > self.large[0]):
+         val = -1 * heapq.heappop(self.small)
+         heapq.heappush(self.large, val)
+
+         # uneven size?
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+        
+        
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        if len(self.large) > len(self.small):
+            return self.large[0]
+        
+        return (-1 * self.small[0] + self.large[0]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
