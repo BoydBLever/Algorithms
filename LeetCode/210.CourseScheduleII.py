dd1d8f3d74f48e01f84c79d22b5fb22be410603c
Boyd Lever <boydlever@stanfordhacker.com>
2023-10-16 21:36:07 -0700

    210. Course Schedule II

---
+++ LeetCode/210.CourseScheduleII.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/course-schedule-ii/
+# NeetCode solution
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # build adjacency list of preques
+        prereq = { c:[] for c in range(numCourses)}
+        for crs, pre in prerequisites:
+            prereq[crs].append(pre)
+
+        # a course has 3 possible states:
+        # visited -> crs has been added to output
+        # visiting -> crs not added to output, but added to cycle
+        # unvisited -> crs not added to output or cycle
+
+        output = []
+        visit, cycle = set(), set()
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visit:
+                return True
+
+            cycle.add(crs)
+            for pre in prereq[crs]:
+                if dfs(pre) == False:
+                    return False
+            cycle.remove(crs)
+            visit.add(crs)
+            output.append(crs)
+            return True
+
+        for c in range(numCourses):
+            if dfs(c) == False:
+                return []
+        return output
