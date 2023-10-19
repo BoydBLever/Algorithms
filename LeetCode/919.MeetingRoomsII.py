f78bb3bc8a61c07a38d28daae0376425f9e29e9c
Boyd Lever <boydlever@stanfordhacker.com>
2023-10-19 11:33:11 -0700

    919. Meeting Rooms II

---
+++ LeetCode/919.MeetingRoomsII.py
@@ -0,0 +1,37 @@
+# https://www.lintcode.com/problem/919/
+# NeetCode solution
+from typing import (
+    List,
+)
+from lintcode import (
+    Interval,
+)
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: the minimum number of conference rooms required
+    """
+    def min_meeting_rooms(self, intervals: List[Interval]) -> int:
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+
+        res, count = 0, 0
+        s, e = 0, 0
+        while s < len(intervals):
+            if start[s] < end[e]:
+                s += 1
+                count += 1
+            else:
+                e += 1
+                count -= 1
+            res = max(res, count)
+        return res
