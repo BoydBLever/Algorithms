a4a3ce5648e2db6468cc53d57ad09368e3a222de
Boyd Lever <boydlever@stanfordhacker.com>
2023-10-20 10:27:19 -0700

    518. Coin Change II

---
+++ LeetCode/518.CoinChangeII.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/coin-change-ii/
+# NeetCode solution
+from typing import List
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        cache = {}
+
+        def dfs(i, a):
+            if a == amount:
+                return 1
+            if a > amount:
+                return 0
+            if i == len(coins):
+                return 0
+            if (i, a) in cache:
+                return cache[(i, a)]
+
+            cache[(i, a)] = dfs(i, a + coins[i]) + dfs(i + 1, a)
+            return cache[(i, a)]
+
+        return dfs(0, 0)
