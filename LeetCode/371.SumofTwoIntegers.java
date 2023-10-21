68e5eb786f2a07de7cd34750af93d141471c9c46
Boyd Lever <boydlever@stanfordhacker.com>
2023-10-20 19:11:15 -0700

    371. Sum of Two Integers

---
+++ LeetCode/371.SumofTwoIntegers.java
@@ -0,0 +1,12 @@
+// NeetCode solution
+// https://leetcode.com/problems/sum-of-two-integers/
+class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int tmp = (a & b) << 1;
+            a = a ^ b;
+            b = tmp;
+        }
+        return a;
+    }
+}
