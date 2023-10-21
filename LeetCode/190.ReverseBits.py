8801db84792f757e820d7b224628b52428793d96
Boyd Lever <boydlever@stanfordhacker.com>
2023-10-20 18:43:37 -0700

    190. Reverse Bits

---
+++ LeetCode/190.ReverseBits.py
@@ -0,0 +1,10 @@
+# NeetCode solution
+# https://leetcode.com/problems/reverse-bits/
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+
+        for i in range(32):
+            bit = (n >> i) & 1
+            res = res | (bit << (31 - i))
+        return res
