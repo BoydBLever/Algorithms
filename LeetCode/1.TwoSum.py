008909ed9e9826a5307831f5dbc840f80ce38671
Boyd Lever <boydlever@stanfordhacker.com>
2023-10-23 08:53:21 -0700

    Update TwoSum.py

--- LeetCode/1.TwoSum.py
+++ LeetCode/1.TwoSum.py
@@ -4,8 +4,8 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
     # check for combinations that sum up to target
-    # hashmap, mapping value to the index (val : index)
-        prevMap = {} # val : index
+    # hashmap, map the number to its index
+        prevMap = {} # val : index #Dictionaries in Python are implementations of hashmaps / hash tables. Store the number from the nums list as the key, and its index as the value.
 
         for i, n in enumerate(nums):
             diff = target - n
