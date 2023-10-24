b045c8dca60350824cbaae3a34a07bd89d74b4be
Boyd Lever <boydlever@stanfordhacker.com>
2023-10-24 11:33:23 -0700

    Stack

---
+++ NeetCode/Stacks.py
@@ -0,0 +1,10 @@
+# Implementing a stack is trivial using a dynamic array
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, n):
+        self.stack.append(n)
+
+    def pop(self):
+        return self.stack.pop()
