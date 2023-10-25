3c99b190b0ebba263593e4abed720faaf1dc3912
Boyd Lever <boydlever@stanfordhacker.com>
2023-10-24 20:28:37 -0700

    206. Reverse Linked List

---
+++ NeetCode/206.ReverseLinkedList.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional, ListNode
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        prev = None #prev pointer is set to null
+        curr = head # curr pointer is set to head
+        # prev, curr = None, head
+        next = head.next
+        while curr:
+            curr.next = prev
+            prev = curr
+            curr = next
+            if next:
+                next = next.next
+        return prev
+    # Time: O(n) Space: O(1)
+
+    # Recursive Solution
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        p = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return p
+    # Time: O(n) Space: O(n)
