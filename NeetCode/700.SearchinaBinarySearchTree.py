17c6e02e4fdfb93877f42d347930d4d7687c8386
Boyd Lever <boydlever@stanfordhacker.com>
2023-10-28 17:02:25 -0700

    700.SearchinaBinarySearchTree.py

---
+++ NeetCode/700.SearchinaBinarySearchTree.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/search-in-a-binary-search-tree/
+# Definition for a binary tree node.
+
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+            
+            if root is None:
+                return None
+            
+            if root.val == val:
+                return root
+            
+            if root.val > val:
+                return self.searchBST(root.left, val)
+            
+            if root.val < val:
+                return self.searchBST(root.right, val)
+            
+            return None
