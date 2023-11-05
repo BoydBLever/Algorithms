c1e50aa138493fcbfa7ec02a7e7acd6611747155
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-05 09:16:59 -0800

    Solution 2, optimized

--- LeetCode/70.ClimbingStairs_repeat.py
+++ LeetCode/70.ClimbingStairs_repeat.py
@@ -18,3 +18,14 @@ class Solution:
     # So dp[i] = dp[i-2] + dp[i-1]
     # dp[1] = 1, dp[2] = 2, dp[3] = 3, dp[4] = 5, dp[5] = 8, dp[6] = 13, dp[7] = 21, dp[8] = 34, dp[9] = 55, dp[10] = 89
     
+    # Solution 2:
+    class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        a, b = 1, 2  # corresponds to dp[1] and dp[2]
+        for i in range(3, n + 1):
+            a, b = b, a + b  # this is equivalent to dp[i] = dp[i - 1] + dp[i - 2]
+        return b  # b is the last computed value, which corresponds to dp[n]
+    # Time: O(n)
+    # Space: O(1)
