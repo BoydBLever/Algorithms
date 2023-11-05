2763839af3560cb875d546134ad9fdca7257997a
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-05 12:34:55 -0800

    121. Best Time to Buy and Sell Stock

---
+++ LeetCode/121.BestTimetoBuyandSellStock_repeat.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Greedy Pattern
+        # if prices is None or len(prices) == 0:
+        if not prices:
+            return 0
+        # variable to store the minimum price
+        min_price = prices[0]
+        # variable to store the maximum profit
+        max_profit = 0
+        # iterate through the prices
+        for i in range(1,len(prices)):
+        # if the current price is less than the minimum price
+            if prices[i] < min_price:
+        # update the minimum price
+                min_price = prices[i]
+            else:
+        # commpute the profit by subtracting the current price from the minimum price
+                max_profit = max(max_profit, prices[i]-min_price)
+        return max_profit
+    
+    # Time: O(n)
+    # Space: O(1)
