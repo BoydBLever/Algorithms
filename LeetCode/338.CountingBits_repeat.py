b00b6a6aca25cc31f942cfb6370eb76e114bc409
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-05 17:38:13 -0800

    Update to Commit Bits

--- LeetCode/338.CountingBits_repeat.py
+++ LeetCode/338.CountingBits_repeat.py
@@ -1,8 +1,8 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        result = [0] * (2 + 1)  # [0, 0, 0]
+        result = [0] * (n + 1)  # [0, 0, 0]
 
-        for i in range(2 + 1):  # i will be 0, then 1, then 2
+        for i in range(n + 1):  # i will be 0, then 1, then 2
             count = 0  # Start count at 0 for each number
             number = i  # Set number to be the current i
             while number > 0:  # While there is a number to check
@@ -12,4 +12,5 @@ class Solution:
             result[i] = count  # Set the count for this number in the result
 
         return result
-    # result should be [0, 1, 1]
+    # Time: O(N * K) where N is the number of integers to check and K is the number of bits in each integer
+    # Space: O(1) since we are not using any extra space to store the result
