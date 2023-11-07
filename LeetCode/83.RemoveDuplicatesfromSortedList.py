7d222860fb8d4b1fdbbf59eb39f7e16705e008b1
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-07 14:31:34 -0800

    Added comments to 83.RemoveDuplicatesfromSortedList

--- LeetCode/83.RemoveDuplicatesfromSortedList.py
+++ LeetCode/83.RemoveDuplicatesfromSortedList.py
@@ -5,8 +5,10 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+            # It means just one thing if the head is empty: there is no list, no nodes, nothing to modify or delete.
             if not head:
                 return None
+            # Why create a dummy node? Because we need to return the head of the list, and the head could be the first duplicate node, so we need to create a dummy node to point to the head of the list.
             dummy = ListNode(0, head)
             cur = dummy
             while cur.next and cur.next.next:
