5f90c7679e5522be08f3dcd0f0fd00aa4cc0ea52
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-07 19:29:07 -0800

    Added comments to 21.MergeTwoSortedLists.py

--- LeetCode/21.MergeTwoSortedLists.py
+++ LeetCode/21.MergeTwoSortedLists.py
@@ -9,32 +9,41 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # Initialize a dummy node and set current to dummy
-        dummy = ListNode()
-        current = dummy
+        # Handle the edge cases
+        if not list1:
+            return list2
+        if not list2:
+            return list1
         
-        # Traverse both lists until one of them is exhausted
+        # Create a dummy node to start the merged list
+        head = ListNode()
+        cur = head # Pointer to the dummy node. Continually append to cur without losing reference to the head of the merged list
+        
+        # Compare the values of the current nodes of list1 and list2. 
         while list1 and list2:
-            # If list1's value is smaller or equal, attach it to current and move list1's pointer
+            # If list1's value is smaller, attach it to cur and move list1's pointer
             if list1.val <= list2.val:
-                current.next = list1
+                # attach list1's current node to the end of the merged list
+                cur.next = list1
+                # After attaching the smaller node to the merged list, you advance the pointer for the list from which you took the node
                 list1 = list1.next
-            # If list2's value is smaller, attach it to current and move list2's pointer
+            # If list2's value is smaller, attach it to cur and move list2's pointer
             else:
-                current.next = list2
+                # Attach list2's current node to the end of the merged list
+                cur.next = list2
+                # After attaching the smaller node to the merged list, you advance the pointer for the list from which you took the node
                 list2 = list2.next
-            # Move the current pointer
-            current = current.next
-        
+            # Move the cur pointer forward to the node that was just attached to the merged list. This is done so that we can attach the next node to the end of the merged list.
+            cur = cur.next
+        # Termination conditions after the while loop: the loop continues until one of the lists is exhausted, meaning one of the pointers reaches the end of the list, becoming None. The lists may be of different lengths so we need to check and make sure every node is attached to the merged list.
         # If list1 is not exhausted, attach the remaining nodes
         if list1:
-            current.next = list1
+            cur.next = list1
         # If list2 is not exhausted, attach the remaining nodes
         else:
-            current.next = list2
-        
-        # Return the merged list starting from the next of the dummy node
-        return dummy.next
+            cur.next = list2
+        # Return the merged list
+        return head.next
 
 #Neetcode solution
     # def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
--- LeetCode/83.RemoveDuplicatesfromSortedList.py
+++ LeetCode/83.RemoveDuplicatesfromSortedList.py
@@ -8,15 +8,19 @@ class Solution:
             # It means just one thing if the head is empty: there is no list, no nodes, nothing to modify or delete.
             if not head:
                 return None
-            # Why create a dummy node? Because we need to return the head of the list, and the head could be the first duplicate node, so we need to create a dummy node to point to the head of the list.
+            # Why create a dummy node? Because we need to return the head of the list, and the head could be the first duplicate node, so we can create a dummy node to point to the head of the list. The dummy is not part of the original list and is used to handle the edge case of removing duplicates at the head without special conditions.
             dummy = ListNode(0, head)
             cur = dummy
+            # We need to check if the current node (cur.next) and the next node (cur.next.next) exist, because if they don't exist, we can't compare their values.
+            # the while loop check if there is a node after the current one (cur.next), and a node after that one as well (cur.next.next)
             while cur.next and cur.next.next:
+                # This means that the node immediately after cur is a duplicate.
                 if cur.next.val == cur.next.next.val:
+                # Remove the link to the immediate next node (cur.next). The pointer that was pointing to the duplicate node now points to the node after the duplicate.
                     cur.next = cur.next.next
                 else:
                     cur = cur.next
-            
+            # Return the head of the list, which is the node after the dummy node.
             return dummy.next
     # Time complexity: O(N) where N is the length of the list
     # Space complexity: O(1)
