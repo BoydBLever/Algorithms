6a0c9053fed1e54b8823f4c1bbf4cd6eb3ccf135
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-11 19:17:02 -0800

    Add comments

--- LeetCodePatterns/111.MinimumDepthofBinaryTree.py
+++ LeetCodePatterns/111.MinimumDepthofBinaryTree.py
@@ -1,15 +1,18 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import Optional, deque
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-            
+            # BFS solution visits nodes level by level, starting from the root. Time: O(N), Space: O(N) 
+            # Edge case: Check if the tree is empty. If it is, then return 0 because the depth of an empty tree is defined as 0.
             if not root:
                 return 0
-    
+            # Initialize a queue. Append the root node to the queue. Initialize depth to 1. The purpose of using a queue is to store all the nodes of a particular level before moving on to the next level. The deque data structure is ideal for this purpose because it supports insertion and deletion from both the ends.
             queue = deque()
             queue.append(root)
             depth = 1
