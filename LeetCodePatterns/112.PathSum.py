bfc69488f69c22061a877e06b1ae65df37a103cd
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-13 18:00:17 -0800

    Addded comments

--- LeetCodePatterns/112.PathSum.py
+++ LeetCodePatterns/112.PathSum.py
@@ -7,3 +7,25 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+            # Base case 
+            if root is None:
+                return False
+            # Check if the root is a leaf node, if so, check if the targetSum is equal to the root value
+            if root.left is None and root.right is None:
+                return root.val == targetSum
+            # Recursively call the function on the left and right subtrees
+            return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
+    # Time complexity: O(N) where N is the number of nodes in the tree
+    # Space complexity: O(N) where N is the number of nodes in the tree
+    # Example 1: Binary Tree
+    #          5
+    #        /   \
+    #       4     8
+    #      /     / \
+    #     11    13  4
+    #    /  \        \
+    #   7    2        1
+    # targetSum = 22
+    # Output: True because 5->4->11->2 sums to 22
+    # Here's how the recursive calls in the algorithm would process this:
+    # 1. First Call: hasPathSum(root=5, targetSum=22)
