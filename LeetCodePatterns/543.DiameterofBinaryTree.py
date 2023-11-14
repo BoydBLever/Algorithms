c990725867b40981e4682bc148413955c3d65817
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-13 20:33:18 -0800

    617. Merge Two Binary Trees

--- LeetCodePatterns/543.DiameterofBinaryTree.py
+++ LeetCodePatterns/543.DiameterofBinaryTree.py
@@ -10,8 +10,10 @@ class Solution:
             def dfs(node):
                 if not node:
                     return 0
+                # What is the height of the binary tree rooted at the left child of the current node ('node')?
                 left = dfs(node.left)
                 right = dfs(node.right)
+                
                 self.ans = max(self.ans, left + right)
                 return max(left, right) + 1
     
---
+++ LeetCodePatterns/617.MergeTwoBinaryTrees.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from TreeNode import TreeNode
+# https://leetcode.com/problems/merge-two-binary-trees/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        # Base cases
+        if root1 is None and root2 is None:
+            return None
+        
+        if root1 is None:
+            return root2
+        if root2 is None:
+            return root1
+        # Merging nodes
+        root1.val += root2.val
+        root1.left = self.mergeTrees(root1.left, root2.left)
+        root1.right = self.mergeTrees(root1.right, root2.right)
+        return root1
