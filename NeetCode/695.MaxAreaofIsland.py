47443a5227128db50f71eea9641dc89a0633044a
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-14 20:25:50 -0800

    695. Max Area of Island

--- NeetCode/695.MaxAreaofIsland.py
+++ NeetCode/695.MaxAreaofIsland.py
@@ -1,20 +1,19 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+
         def dfs(r, c):
-            # base case
-            if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]):
+            if (r < 0 or r == ROWS or c < 0 or c == COLS or grid[r][c] == 0 or (r, c) in visit):
                 return 0
-    
-            if grid[r][c] == 0:
-                return 0
-            
-            grid[r][c] = 0
+        
+            visit.add((r, c))
             return 1 + dfs(r-1, c) + dfs(r+1, c) + dfs(r, c-1) + dfs(r, c+1)
         
-        max_area = 0
+        area = 0
         # Visit every single position in the grid. Let's iterate through each row and column.
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 # If we find a 1, then we have found an island. Let's DFS from that position.
-                max_area = max(max_area, dfs(r, c))
-        return max_area
+                area = max(area, dfs(r, c))
+        return area
