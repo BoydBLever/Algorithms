bebc50642831bf9486582ad4611bfbe26f27569f
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-16 21:30:29 -0800

    572. Subtree of Another Tree

---
+++ LeetCodePatterns/572.SubtreeofAnotherTree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+            
+            def isSameTree(s, t):
+                if not s and not t:
+                    return True
+                elif not s or not t:
+                    return False
+                elif s.val != t.val:
+                    return False
+                else:
+                    return isSameTree(s.left, t.left) and isSameTree(s.right, t.right)
+            
+            if not root:
+                return False
+            elif isSameTree(root, subRoot):
+                return True
+            else:
+                return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
--- LeetCodePatterns/617.MergeTwoBinaryTrees.py
+++ LeetCodePatterns/617.MergeTwoBinaryTrees.py
@@ -12,12 +12,11 @@ class Solution:
         # Base cases
         if root1 is None and root2 is None:
             return None
-        
         if root1 is None:
             return root2
         if root2 is None:
             return root1
-        # Merging nodes
+        # Merge nodes
         root1.val += root2.val
         root1.left = self.mergeTrees(root1.left, root2.left)
         root1.right = self.mergeTrees(root1.right, root2.right)
