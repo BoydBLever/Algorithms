f2082beb51f766efc80407691694d51f06df55a8
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-17 18:33:57 -0800

    977. Squares of a Sorted Array

---
+++ LeetCodePatterns/977.SquaresofaSortedArray.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+    # Brute Force
+        return sorted([i**2 for i in nums])
+    
+    # Two Pointer
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        res = [0] * len(nums)
+        left, right = 0, len(nums) - 1
+        insert_pos = len(nums) - 1
+
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                res[insert_pos] = nums[left] ** 2
+                left += 1
+            else:
+                res[insert_pos] = nums[right] ** 2
+                right -= 1
+            insert_pos -= 1
+
+        return res                             
