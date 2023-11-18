2b72d2b849431291edc30a0bf436177a1b5838f9
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-18 14:58:38 -0800

    238. Product of Array Except Self

--- LeetCodePatterns/643.MaximumAverageSubarrayI.py
+++ LeetCodePatterns/643.MaximumAverageSubarrayI.py
@@ -1,13 +1,11 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         # Initial check
-        if len(nums) < k:
-            return 0
+        if len(nums) < k: return 0
         # Initialize the sum
         sum = 0
         # Iterate k times to get the sum of the first k elements
-        for i in range(k):
-            sum += nums[i]
+        for i in range(k): sum += nums[i]
         # Initialize the max sum
         maxSum = sum
         # Iterate from k to the end of the array
