8af68469fc3a48895bb9738103992950a2949e4a
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-19 14:42:56 -0800

    48. Rotate Image

---
+++ LeetCode/48.RotateImage_repeat.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        # 1. Matrix Transposition
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        # 2. Reverse the order of the elements in each row of the matrix
+        for i in range(n):
+            matrix[i].reverse()
