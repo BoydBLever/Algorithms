47def53a67abe2c5f94e603bab9eb2cfbd730851
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-25 20:16:12 -0800

    2947. Count Beautiful Substrings I

--- LeetCode/100132.CountBeautifulSubstringsII.py
+++ LeetCode/100132.CountBeautifulSubstringsII.py
@@ -1,19 +1,20 @@
+# Credits to SalvadorDali
 class Solution:
-    def beautifulSubstrings(self, s: str, k: int) -> int:
-        count = 0
-        vowels = "aeiou"
+  def beautifulSubstrings(self, s: str, k: int) -> int:
+    cnt_0 = [0] * (len(s) + 1)
+    cnt_1 = [0] * (len(s) + 1)
+    for i, c in enumerate(s):
+      if c in ['a', 'e', 'i', 'o', 'u']:
+        cnt_0[i + 1] = cnt_0[i] + 1
+        cnt_1[i + 1] = cnt_1[i]
+      else:
+        cnt_0[i + 1] = cnt_0[i]
+        cnt_1[i + 1] = cnt_1[i] + 1
 
-        # Helper function to check if a substring is beautiful
-        def is_beautiful(sub):
-            vowel_count = sum(1 for char in sub if char in vowels)
-            consonant_count = len(sub) - vowel_count
-            # Check if the product of vowels and consonants is divisible by k
-            return vowel_count * consonant_count % k == 0 if vowel_count * consonant_count > 0 else False
-        
-        # Check all possible substrings
-        for i in range(len(s)):
-            for j in range(i+1, len(s)+1):
-                if is_beautiful(s[i:j]):
-                    count += 1
-        
-        return count
+    res = 0
+    for i in range(len(s)):
+      for j in range(i + 1, len(s) + 1):
+        v1, v2 = cnt_0[j] - cnt_0[i], cnt_1[j] - cnt_1[i]
+        if v1 == v2 and v1 * v2 % k == 0:
+          res += 1
+    return res
