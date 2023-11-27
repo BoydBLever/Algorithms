abaf91f9e9c3931a6ffffae7dd16d19f773377dd
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-26 18:30:48 -0800

    Add Biran Kernighan's algorithm

--- NeetCode/191.Numberof1bits.py
+++ NeetCode/191.Numberof1bits.py
@@ -7,3 +7,13 @@ class Solution:
                 count += 1
             n = n >> 1
         return count
+
+# Brian Kernighan’s Algorithm
+# Iterate only over the bits that are set to 1
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            n &= n - 1
+            count += 1
+        return count
