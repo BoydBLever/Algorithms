9394b8d95fec40ce52c4f6221deb69ea57f3f0cd
Boyd Lever <boydlever@stanfordhacker.com>
2023-11-29 21:43:46 -0800

    1611. Minimum One Bit Operations to Make Integers Zero

--- LeetCode/236.LowestCommonAncestorofaBinaryTree.py
+++ LeetCode/236.LowestCommonAncestorofaBinaryTree.py
@@ -1,10 +1,10 @@
 # https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
@@ -24,5 +24,16 @@ class Solution:
                     return left
     
             return dfs(root, p, q)
-    # Time: O(N)
-    # Space: O(N)
+# Time complexity: O(N)
+# Space complexity: O(N)
+# root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+# Output: 3
+# Tree
+#         3
+#       /   \
+#      5     1
+#     / \   / \
+#    6   2 0   8
+#       / \
+#      7   4
+# The term "lowest" refers to the deepest level in the tree where the paths from the root to p and q meet.
